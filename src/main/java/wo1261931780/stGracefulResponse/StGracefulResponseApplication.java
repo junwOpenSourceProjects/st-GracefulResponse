46f465320611b3be12b6701d957c1be5a144e699
junw <wo1261931780@gmail.com>
2024-05-14 18:41:52 +0800

    初始化

---
+++ src/main/java/wo1261931780/stGracefulResponse/StGracefulResponseApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.stGracefulResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StGracefulResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StGracefulResponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/stGracefulResponse/StGracefulResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.stGracefulResponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StGracefulResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
